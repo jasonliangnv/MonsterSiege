32670627572dce76cdb42b53201716f542b11f9e
Dieu_John <jdieu@nevada.unr.edu>
2023-04-25 22:18:43 -0700

    WaveSpawner, GoblinSprite, Linked to UI
    Added the WaveSpawner to the GameManager, adjusted prefab sizes, added Wave class to hold editable wave data, linked wave data to UI

---
+++ Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class WaveSpawner : MonoBehaviour
+{
+    public static int EnemiesAlive = 0;
+
+    //array of defined wave types to give to wavespawner
+    public Wave[] waves;
+
+    public Transform spawnPoint;
+
+    public TMP_Text waveCountText;
+
+    public Image waveUnitIcon;
+    public TMP_Text waveUnitNumber;
+
+    public float timeBetweenWaves = 5f;
+    private float countdown = 2f;
+
+    private int waveNumber = 0;
+    void Update()
+    {
+        Wave upcomingWave = waves[waveNumber];
+        waveUnitIcon.sprite = upcomingWave.icon;
+        waveUnitNumber.text = upcomingWave.count.ToString();
+
+        waveCountText.text = "Wave: " + (waveNumber + 1).ToString();
+        if(countdown <= 0f)
+        {
+            StartCoroutine(SpawnWave());
+            countdown = timeBetweenWaves;
+        }
+
+        countdown -= Time.deltaTime;
+    }
+
+    IEnumerator SpawnWave()
+    {
+        //takes wave in wave array, spawns enemies according to parameters
+        Wave wave = waves[waveNumber];
+
+        for (int i = 0; i < wave.count; i++)
+        {
+            SpawnEnemy(wave.enemy);
+            yield return new WaitForSeconds(1f / wave.rate);
+        }
+
+        waveNumber++;
+
+        if(waveNumber == waves.Length)
+        {
+            //end level
+        }
+    }
+
+    void SpawnEnemy(GameObject enemy)
+    {
+        Instantiate(enemy, spawnPoint.position, spawnPoint.rotation);
+        //Need to add EnemiesAlive-- to enemy scripts
+        //EnemiesAlive++;
+    }
+}
