7478c1c59f44f2f482cb7ed94c147e2d48a9ba66
Dieu_John <jdieu@nevada.unr.edu>
2023-05-02 21:15:32 -0700

    Font, UI,  Node Overlap

--- Assets/Scripts/Node.cs
+++ Assets/Scripts/Node.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class Node : MonoBehaviour
 {
@@ -56,6 +57,11 @@ public class Node : MonoBehaviour
 
     void OnMouseEnter()
     {
+        if (EventSystem.current.IsPointerOverGameObject())
+        {
+            return;
+        }
+
         if (buildManager.GetUnitToBuild() == null)
         {
             return;
