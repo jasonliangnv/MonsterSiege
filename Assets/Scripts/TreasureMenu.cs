a093370139a103871249ebb2fd0618655b8e7495
Dieu_John <jdieu@nevada.unr.edu>
2023-05-11 22:50:55 -0700

    Treasure Selection fixed, not hard coded.

--- Assets/Scripts/TreasureMenu.cs
+++ Assets/Scripts/TreasureMenu.cs
@@ -4,50 +4,55 @@ using UnityEngine;
 
 public class TreasureMenu : MonoBehaviour
 {
-    public GameObject[] treasures;
+    public List<GameObject> treasures;
     public GameObject treasure1;
     public GameObject treasure2;
     public GameObject treasure3;
+
+    private GameObject panel;
 
     void Awake()
     {
         //treasures = Resources.LoadAll("Prefabs/Treasures", typeof(GameObject));
     }
 
+
     // Start is called before the first frame update
     void Start()
     {
+        panel = GameObject.Find("Treasures");
+        
         // Saves value of our random treasure
-        int temp = Random.Range(0, (treasures.Length - 1));
+        int temp = Random.Range(0, (treasures.Count - 1));
         
         // Saves the treasure and then removes it from the collection
         GameObject selected = treasures[temp];
-        //treasures.RemoveAt(temp);
+        treasures.RemoveAt(temp);
 
         // Instantiates the treasure
         treasure1 = selected;
-        treasure1.SetActive(true);        
+        Instantiate(treasure1, panel.transform);    
 
         // Saves value of our random treasure
-        temp = Random.Range(0, (treasures.Length - 1));
+        temp = Random.Range(0, (treasures.Count - 1));
 
         // Saves the treasure and then removes it from the collection
         selected = treasures[temp];
-        //treasures.RemoveAt(temp);
+        treasures.RemoveAt(temp);
 
         // Instantiates the treasure
         treasure2 = selected;
-        treasure2.SetActive(true);
+        Instantiate(treasure2, panel.transform);
 
         // Saves value of our random treasure
-        temp = Random.Range(0, (treasures.Length - 1));
+        temp = Random.Range(0, (treasures.Count - 1));
         
         // Saves the treasure and then removes it from the collection
         selected = treasures[temp];
-        //treasures.RemoveAt(temp);
+        treasures.RemoveAt(temp);
 
         // Instantiates the treasure
         treasure3 = selected;
-        treasure3.SetActive(true);
+        Instantiate(treasure3, panel.transform);
     }
 }
